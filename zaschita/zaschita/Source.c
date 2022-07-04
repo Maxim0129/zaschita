b2ae87aee069b958be42811935b3d58a0e357b66
Maxim Kotkovets <dogemax@mail.ru>
2022-07-04 20:46:47 +0300

    this is main

---
+++ zaschita/zaschita/Source.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#include<windows.h>
+#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define size 8
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+	int arr[size] = { 1, 2, 3, 4, 5, 6, 8, 2 };
+
+	printf("Исходный массив ");
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	int max = arr[0];
+	int nm = 0;
+	for (int i = 1; i < size; i++)
+	{
+		if (arr[i] >= max)
+		{
+			max = arr[i];
+			nm = i;
+		}
+	}
+	printf("Максимальный элемент max[%d] = %d\n", nm + 1, max);
+	puts("Измененный массив");
+	for (int i = 0; i < size; i++)
+	{
+		arr[4] = max;
+		arr[nm] = -1;
+		printf("%d ", arr[i]);
+	}
+}
